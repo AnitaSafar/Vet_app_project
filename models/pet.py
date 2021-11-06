d41df0dc8efa3aee1595d583dbbc97a3231a55c2
Anita Safar <ancsa0416@gmail.com>
2021-11-06 12:52:47 +0000

    classes created

---
+++ models/pet.py
@@ -0,0 +1,10 @@
+class Pet:
+
+    def __init__(self, name, date_of_birth, type, owner, notes, id = None):
+        self.name = name
+        self.date_of_birth = date_of_birth
+        self.type = type
+        self.owner = owner
+        self.notes = notes
+        self.id = id
+        
